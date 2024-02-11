afbb10c296587615526b4b8da948738ee5e326ff
VitorHenriqueSilvaDeSa <vitorhenriquesilvadesa@gmail.com>
2024-02-11 19:58:08 -0300

    Basic entity class.

--- src/com/vgames/survivalreckoning/entity/Entity.java
+++ src/com/vgames/survivalreckoning/entity/Entity.java
@@ -1,46 +1,9 @@
 package com.vgames.survivalreckoning.entity;
 
-import com.vgames.survivalreckoning.entity.component.Component;
-
-import java.lang.reflect.InvocationTargetException;
-import java.util.HashMap;
-import java.util.Map;
 
 public abstract class Entity {
 
-    private Map<Class<? extends Component>, Component> components;
-
     public Entity() {
-        this.components = new HashMap<>();
-    }
 
-    public <T extends Component> Component addComponent(Class<T> componentClass) {
-        T component;
-        try {
-            component = componentClass.getDeclaredConstructor().newInstance();
-            this.components.put(componentClass, component);
-        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
-            throw new RuntimeException(e);
-        }
-
-        return component;
-    }
-
-    @SuppressWarnings("unchecked")
-    public <T extends Component> T getComponent(Class<T> componentClass) {
-        return (T) this.components.get(componentClass);
-    }
-
-    @SuppressWarnings("unchecked")
-    public <T extends Component> T removeComponent(Class<T> componentClass) {
-        Component component = this.components.get(componentClass);
-        this.components.remove(componentClass);
-        return (T) component;
-    }
-
-    public void update() {
-        for(Component component : this.components.values()) {
-            component.update();
-        }
     }
 }
